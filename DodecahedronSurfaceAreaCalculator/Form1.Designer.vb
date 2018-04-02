7741ac4d100a0a4c6920d801866b4be752fcd80b
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-03 03:31:02 +0800

    Initial Commit

---
+++ DodecahedronSurfaceAreaCalculator/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.edgeBox = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.areaResult = New System.Windows.Forms.Label()
+        Me.lengthSelection = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'edgeBox
+        '
+        Me.edgeBox.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.edgeBox.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.edgeBox.Location = New System.Drawing.Point(282, 144)
+        Me.edgeBox.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.edgeBox.Mask = "00000"
+        Me.edgeBox.Name = "edgeBox"
+        Me.edgeBox.Size = New System.Drawing.Size(155, 29)
+        Me.edgeBox.TabIndex = 0
+        Me.edgeBox.ValidatingType = GetType(Integer)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.Label1.Location = New System.Drawing.Point(204, 23)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(293, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Solve for Surface Area in meters"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DimGray
+        Me.Label2.Location = New System.Drawing.Point(11, 198)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(242, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "© Luis Edward M. Miranda - 2017"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(204, 144)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(73, 30)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(173, 162)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Button1.Font = New System.Drawing.Font("Tekton Pro Ext", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(404, 189)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 34)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(199, 116)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Enter Value:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(203, 62)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 33)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "A ≈"
+        '
+        'areaResult
+        '
+        Me.areaResult.AutoSize = True
+        Me.areaResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.areaResult.ForeColor = System.Drawing.Color.Black
+        Me.areaResult.Location = New System.Drawing.Point(265, 62)
+        Me.areaResult.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.areaResult.Name = "areaResult"
+        Me.areaResult.Size = New System.Drawing.Size(273, 33)
+        Me.areaResult.TabIndex = 9
+        Me.areaResult.Text = "3×√(25+10×√5) × a²"
+        '
+        'lengthSelection
+        '
+        Me.lengthSelection.Font = New System.Drawing.Font("Times New Roman", 14.25!)
+        Me.lengthSelection.FormattingEnabled = True
+        Me.lengthSelection.Items.AddRange(New Object() {"cm", "ft", "yard", "inches", "km", "meters", "miles", "mm"})
+        Me.lengthSelection.Location = New System.Drawing.Point(442, 144)
+        Me.lengthSelection.Name = "lengthSelection"
+        Me.lengthSelection.Size = New System.Drawing.Size(96, 29)
+        Me.lengthSelection.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(553, 236)
+        Me.Controls.Add(Me.lengthSelection)
+        Me.Controls.Add(Me.areaResult)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.edgeBox)
+        Me.Font = New System.Drawing.Font("OCR A Extended", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Dodecahedron Calculator"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents edgeBox As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents areaResult As Label
+    Friend WithEvents lengthSelection As ComboBox
+End Class
---
+++ DodecahedronSurfaceAreaCalculator/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    '' Initialize Each Data Types
+    Dim area As Double
+    Dim areaInput As Double
+
+
+    Private Sub lengthSelection_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        '' If there is no default selected Item select the first one
+        If lengthSelection.Items.Count > 0 Then
+            lengthSelection.SelectedIndex = 0    ' The first item has index 0 '
+        End If
+    End Sub
+
+    '' Event Listener When the Button Calculate is clicked
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ''This is to avoid an annoying freaking error
+        If edgeBox.Text = "" Then
+            areaInput = 0.0
+        Else
+            areaInput = edgeBox.Text
+        End If
+
+        '' Selected Length is CM
+        If lengthSelection.SelectedIndex = 0 Then
+            areaInput = areaInput / 100
+        End If
+        '' Selected Length is Ft
+        If lengthSelection.SelectedIndex = 1 Then
+            areaInput = areaInput / 3.28083989501
+        End If
+        '' Selected Length is Yard
+        If lengthSelection.SelectedIndex = 2 Then
+            areaInput = areaInput / 1.09361329834
+        End If
+        '' Selected Length is Inches
+        If lengthSelection.SelectedIndex = 3 Then
+            areaInput = areaInput / 39.3700787402
+        End If
+        '' Selected Length is KM
+        If lengthSelection.SelectedIndex = 4 Then
+            areaInput = areaInput / 0.001
+        End If
+        '' Selected Length is Miles
+        If lengthSelection.SelectedIndex = 6 Then
+            areaInput = areaInput / 0.00062137273
+        End If
+        '' Selected Length is MM
+        If lengthSelection.SelectedIndex = 7 Then
+            areaInput = areaInput / 1000
+        End If
+
+
+        '' Calculate the area
+        area = 3 * Math.Sqrt(25 + 10 * Math.Sqrt(5)) * areaInput ^ 2
+
+        '' Put the result in the label Text
+        areaResult.Text = area
+
+    End Sub
+End Class
